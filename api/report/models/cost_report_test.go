34523a411df43d21550cdf6ac977135e4c61b60c
Nils Gustav Stråbø <nst@equinor.com>
2022-05-25 13:38:24 +0200

    remove commented code

--- api/report/models/cost_report_test.go
+++ api/report/models/cost_report_test.go
@@ -10,22 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// func setup() (*reportUtils.ReportUtils, func()) {
-// 	utils := reportUtils.NewReportTestUtils()
-
-// 	teardown := func() {
-// 		utils.File.Close()
-// 		os.Remove(utils.File.Name())
-// 	}
-
-// 	return &utils, teardown
-// }
-
 func Test_Created_Report_Exists(t *testing.T) {
-
-	// utils, teardown := setup()
-	// defer teardown()
-
 	report := NewCostReport(&models.ApplicationCostSet{
 		ApplicationCosts: []models.ApplicationCost{
 			{Name: "name1"},
@@ -39,7 +24,6 @@ func Test_Created_Report_Exists(t *testing.T) {
 	assert.Nil(t, err)
 
 	// Add assertion that reads the CSV report and verifies the content
-
 	reader := csv.NewReader(fileData)
 	reader.Comma = ';'
 	allContent, err := reader.ReadAll()
